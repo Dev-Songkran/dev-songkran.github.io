2289f373504f576bba4939ba97d5d0040be758db
Dev-Songkran <cronozen@gmail.com>
2017-11-29 15:33:38 +0700

    Update :kiss:

--- source/.next/dist/pages/index.js
+++ source/.next/dist/pages/index.js
@@ -43,36 +43,36 @@ var _Chart2 = _interopRequireDefault(_Chart);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var Container = _styledComponents2.default.div.withConfig({
-    componentId: "s1agfuu7-0"
+    componentId: "v037p7-0"
 })(["max-width:1200px;width:1200px;margin:0 auto;padding:40px;min-height:500px;background:#fff;border-radius:3px;box-shadow:0 1px 1px rgba(0,0,0,0.1);position:relative;"]);
 var Img = _styledComponents2.default.img.withConfig({
-    componentId: "s1agfuu7-1"
+    componentId: "v037p7-1"
 })(["display:block;margin:0 auto;border-radius:100%;"]);
 var Myname = _styledComponents2.default.h3.withConfig({
-    componentId: "s1agfuu7-2"
+    componentId: "v037p7-2"
 })(["margin:10px auto;display:table;font-size:25px;font-weight:400;color:#333;"]);
 var Detail = _styledComponents2.default.p.withConfig({
-    componentId: "s1agfuu7-3"
+    componentId: "v037p7-3"
 })(["font-size:14px;color:#111;display:table;margin:0 auto;font-weight:400;margin-bottom:", "px;margin-top:", "px;a{color:#446594;font-weight:600;}"], function (props) {
     return props.mb ? props.mb : '5';
 }, function (props) {
     return props.mt ? props.mt : '0';
 });
 var Icon = _styledComponents2.default.div.withConfig({
-    componentId: "s1agfuu7-4"
+    componentId: "v037p7-4"
 })(["background-image:", ";background-size:100%;height:30px;width:30px;margin:0 auto;display:inline-block;"], function (props) {
     return props.svg ? "url(" + props.svg + ")" : "url('')";
 });
 var Box = _styledComponents2.default.div.withConfig({
-    componentId: "s1agfuu7-5"
+    componentId: "v037p7-5"
 })(["margin-top:30px;margin-bottom:30px;border-top:1px solid #f197a3;>h3{text-align:center;font-weight:500;}"]);
 var Timeline = _styledComponents2.default.div.withConfig({
-    componentId: "s1agfuu7-6"
+    componentId: "v037p7-6"
 })(["display:flex;position:relative;margin-bottom:15px;&:after,&:before{content:'';position:absolute;left:0;right:0;bottom:0;top:0;margin:auto;}&:after{width:30px;height:30px;display:flex;border-radius:100%;border:2px solid #f197a3;padding:10px;background:#fff;background-image:", ";background-repeat:no-repeat;background-position:center;}&:before{width:2px;background:#f197a3;bottom:auto;height:120%;}"], function (props) {
     return props.svg ? "url(" + props.svg + ")" : "url('')";
 });
 var Text = _styledComponents2.default.div.withConfig({
-    componentId: "s1agfuu7-7"
+    componentId: "v037p7-7"
 })(["width:50%;display:flex;margin-left:", ";margin-right:", ";h3{margin:0;font-weight:400;color:#333;width:100%;}", "{color:#f197a3;margin:0;width:100%;}"], function (props) {
     return props.left ? 'auto' : '30px';
 }, function (props) {
@@ -80,24 +80,24 @@ var Text = _styledComponents2.default.div.withConfig({
 }, Detail);
 
 var Year = _styledComponents2.default.div.withConfig({
-    componentId: "s1agfuu7-8"
+    componentId: "v037p7-8"
 })(["width:50%;margin-left:", ";margin-right:", ";display:flex;padding:0 15px;"], function (props) {
     return props.left ? 'auto' : '30px';
 }, function (props) {
     return props.right ? 'auto' : '30px';
 });
 var Card = _styledComponents2.default.div.withConfig({
-    componentId: "s1agfuu7-9"
+    componentId: "v037p7-9"
 })(["padding:15px;", ";", ";&.custom{padding:5px 15px;height:40px;border:2px solid #785892;border-radius:50px;position:relative;margin-top:auto;margin-bottom:auto;color:#785892;}@media (max-width:670px){h3{font-size:14px;}p{font-size:10px;}&.custom{font-size:13px;padding:5px 10px;height:35px;font-weight:400;}}"], function (props) {
     return props.left ? "margin-left: auto; margin-right:0;text-align:right;" : "";
 }, function (props) {
     return props.right ? "margin-right: auto; margin-left:0;text-align:left;" : "";
 });
 var Skill = _styledComponents2.default.div.withConfig({
-    componentId: "s1agfuu7-10"
+    componentId: "v037p7-10"
 })(["margin:15px;"]);
 var Flex = _styledComponents2.default.div.withConfig({
-    componentId: "s1agfuu7-11"
+    componentId: "v037p7-11"
 })(["display:flex;align-items:center;justify-content:center"]);
 
 var App = function (_Component) {
@@ -112,7 +112,7 @@ var App = function (_Component) {
     (0, _createClass3.default)(App, [{
         key: "render",
         value: function render() {
-            return _react2.default.createElement(Container, null, _react2.default.createElement(Img, { width: "200", height: "200", src: "../static/media/my.1bda1d7a.jpg" }), _react2.default.createElement(Myname, null, "Songkran Wichitpong"), _react2.default.createElement(Detail, null, "I'm a front-end and back-end developer from Thailand"), _react2.default.createElement(Detail, null, _react2.default.createElement("b", null, "address"), " 263 , 14 Lane , Rattana Uthit Rd., Hatyai Sub-district, Hatyai District, Songkhla, 90110 ,Thailand."), _react2.default.createElement(Detail, { mt: "20", mb: "10" }, _react2.default.createElement("b", null, "Email : "), "s.Wichitpong@gmail.com , ", _react2.default.createElement("b", null, "Tel : "), "064-6099309"), _react2.default.createElement(Detail, { mb: "10" }, _react2.default.createElement("b", null, "Github"), " : ", _react2.default.createElement(_link2.default, { href: "https://github.com/Dev-Songkran" }, _react2.default.createElement("a", null, "https://github.com/dev-songkran"))), _react2.default.createElement(Detail, { mb: "10" }, _react2.default.createElement("b", null, "Website"), " : ", _react2.default.createElement(_link2.default, { href: "https://dev-songkran.github.io" }, _react2.default.createElement("a", null, "https://dev-songkran.github.io"))), _react2.default.createElement(Box, null, _react2.default.createElement("h3", null, "EDUCATION AND EXPERIENCE"), _react2.default.createElement(Timeline, { svg: "../static/svg/book.svg" }, _react2.default.createElement(Text, { left: true }, _react2.default.createElement(Card, { left: true }, _react2.default.createElement("h3", null, "Bachelor of Science"), _react2.default.createElement(Detail, null, "University of Hatyai"), _react2.default.createElement(Detail, null, "Songkhla"))), _react2.default.createElement(Year, { right: true }, _react2.default.createElement(Card, { right: true, className: "custom" }, " 2011 - 2015 "))), _react2.default.createElement(Timeline, { svg: "../static/svg/coffe.svg" }, _react2.default.createElement(Year, { left: true }, _react2.default.createElement(Card, { left: true, className: "custom" }, " Jul 2015 - Nov 2015")), _react2.default.createElement(Text, { right: true }, _react2.default.createElement(Card, { right: true }, _react2.default.createElement("h3", null, "Freelance"), _react2.default.createElement(Detail, null, "Front-End Developer, Graphic Designer"), _react2.default.createElement(Detail, null, "Hatyai")))), _react2.default.createElement(Timeline, { svg: "../static/svg/coffe.svg" }, _react2.default.createElement(Text, { left: true }, _react2.default.createElement(Card, { left: true }, _react2.default.createElement("h3", null, "Chiangrai Focus"), _react2.default.createElement(Detail, null, "Front-End & Back-End Developer."), _react2.default.createElement(Detail, null, "Chiangrai"))), _react2.default.createElement(Year, { right: true }, _react2.default.createElement(Card, { right: true, className: "custom" }, " MAY 2016 - IN PROGRESS")))), _react2.default.createElement(Box, { style: { marginTop: '50px' } }, _react2.default.createElement("h3", null, "SKILLS"), _react2.default.createElement(Flex, null, _react2.default.createElement(_Chart2.default, { percent: "96", skillName: "HTML5 / CSS3" }), _react2.default.createElement(_Chart2.default, { percent: "85", skillName: "Javascript" }), _react2.default.createElement(_Chart2.default, { percent: "89", skillName: "PHP" }))));
+            return _react2.default.createElement(Container, null, _react2.default.createElement(Img, { width: "200", height: "200", src: "../static/media/my.1bda1d7a.jpg" }), _react2.default.createElement(Myname, null, "Songkran Wichitpong"), _react2.default.createElement(Detail, null, "I'm a front-end and back-end developer from Thailand"), _react2.default.createElement(Detail, null, _react2.default.createElement("b", null, "address"), " 263 , 14 Lane , Rattana Uthit Rd., Hatyai Sub-district, Hatyai District, Songkhla, 90110 ,Thailand."), _react2.default.createElement(Detail, { mt: "20", mb: "10" }, _react2.default.createElement("b", null, "Email : "), "s.Wichitpong@gmail.com , ", _react2.default.createElement("b", null, "Tel : "), "064-6099309"), _react2.default.createElement(Detail, { mb: "10" }, _react2.default.createElement("b", null, "Github"), " : ", _react2.default.createElement(_link2.default, { href: "https://github.com/Dev-Songkran" }, _react2.default.createElement("a", null, "https://github.com/dev-songkran"))), _react2.default.createElement(Detail, { mb: "10" }, _react2.default.createElement("b", null, "Website"), " : ", _react2.default.createElement(_link2.default, { href: "https://dev-songkran.github.io" }, _react2.default.createElement("a", null, "https://dev-songkran.github.io"))), _react2.default.createElement(Box, null, _react2.default.createElement("h3", null, "EDUCATION AND EXPERIENCE"), _react2.default.createElement(Timeline, { svg: "../static/svg/book.svg" }, _react2.default.createElement(Text, { left: true }, _react2.default.createElement(Card, { left: true }, _react2.default.createElement("h3", null, "Bachelor of Science"), _react2.default.createElement(Detail, null, "University of Hatyai"), _react2.default.createElement(Detail, null, "Songkhla"))), _react2.default.createElement(Year, { right: true }, _react2.default.createElement(Card, { right: true, className: "custom" }, " 2011 - 2015 "))), _react2.default.createElement(Timeline, { svg: "../static/svg/coffe.svg" }, _react2.default.createElement(Year, { left: true }, _react2.default.createElement(Card, { left: true, className: "custom" }, " Jul 2015 - Nov 2015")), _react2.default.createElement(Text, { right: true }, _react2.default.createElement(Card, { right: true }, _react2.default.createElement("h3", null, "Freelance"), _react2.default.createElement(Detail, null, "Front-End Developer, Graphic Designer"), _react2.default.createElement(Detail, null, "Hatyai")))), _react2.default.createElement(Timeline, { svg: "../static/svg/coffe.svg" }, _react2.default.createElement(Text, { left: true }, _react2.default.createElement(Card, { left: true }, _react2.default.createElement("h3", null, "Chiangrai Focus"), _react2.default.createElement(Detail, null, "Front-End & Back-End Developer."), _react2.default.createElement(Detail, null, "Chiangrai"))), _react2.default.createElement(Year, { right: true }, _react2.default.createElement(Card, { right: true, className: "custom" }, " MAY 2016 - IN PROGRESS")))), _react2.default.createElement(Box, { style: { marginTop: '50px' } }, _react2.default.createElement("h3", null, "SKILLS"), _react2.default.createElement(Flex, null, _react2.default.createElement(_Chart2.default, { percent: "96", skillName: "HTML5 / CSS3" }), _react2.default.createElement(_Chart2.default, { percent: "85", skillName: "javascript / jQuery" }), _react2.default.createElement(_Chart2.default, { percent: "89", skillName: "PHP" })), _react2.default.createElement(Flex, null, _react2.default.createElement(_Chart2.default, { percent: "85", skillName: "mySQL" }), _react2.default.createElement(_Chart2.default, { percent: "40", skillName: "React.js / Next.js" }))));
         }
     }]);
 
--- source/pages/index.js
+++ source/pages/index.js
@@ -208,10 +208,11 @@ class App extends Component {
                     <h3>SKILLS</h3>
                     <Flex>
                         <Chart percent="96" skillName="HTML5 / CSS3"></Chart>
-                        <Chart percent="85" skillName="Javascript"></Chart>
+                        <Chart percent="85" skillName="javascript / jQuery"></Chart>
                         <Chart percent="89" skillName="PHP"></Chart>
                     </Flex>
                     <Flex>
+                        <Chart percent="85" skillName="mySQL"></Chart>
                         <Chart percent="40" skillName="React.js / Next.js"></Chart>
                     </Flex>
                 </Box>

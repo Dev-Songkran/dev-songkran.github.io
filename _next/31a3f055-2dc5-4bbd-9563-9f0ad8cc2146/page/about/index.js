f3bdf093725e740fe93c5cf7fcd116642c75df1c
Dev-Songkran <cronozen@gmail.com>
2017-11-29 15:34:48 +0700

    Delete Last Vers. :ok_hand:

--- _next/31a3f055-2dc5-4bbd-9563-9f0ad8cc2146/page/about/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/about', function() {
-            var comp = module.exports=webpackJsonp([5],{212:function(e,t,l){e.exports=l(213)},213:function(e,t,l){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=l(7),r=u(n),a=l(1),f=u(a),d=l(2),o=u(d),c=l(8),i=u(c),s=l(9),p=u(s),_=l(4),m=u(_),h=l(100),v=u(h),E=function(e){function t(){return(0,f.default)(this,t),(0,i.default)(this,(t.__proto__||(0,r.default)(t)).apply(this,arguments))}return(0,p.default)(t,e),(0,o.default)(t,[{key:"render",value:function(){return m.default.createElement("div",null,"Hello World ",m.default.createElement("br",null)," ",m.default.createElement(v.default,{href:"/"},m.default.createElement("a",null,"Back"))," ")}}]),t}(_.Component);t.default=E}},[212]);
-            return { page: comp.default }
-          })
-        
